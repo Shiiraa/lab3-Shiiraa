22c0677d11fc6ee2562d34e618df87da98cb0162
4010b5 <4010b5@mango.ece.uprm.edu>
2017-02-08 10:47:11 -0400

    Added a new Class that paints the frame

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -6,12 +6,16 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Alanis Y. Nazario");
-		
-		
-		
+
+
+
 		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setLocation(880, 380);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+
+
+		MyPanelClass myPanel = new MyPanelClass();
+		myFrame.getContentPane().add(myPanel);
 	}
 }
