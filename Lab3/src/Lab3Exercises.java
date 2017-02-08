975591ddc5c0864f9087877838a376baac92a679
4010b5 <4010b5@mango.ece.uprm.edu>
2017-02-08 10:34:56 -0400

    Set Location and Changed Default Close operation

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,17 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Alanis Y. Nazario");
+		
+		
+		
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setLocation(880, 380);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
